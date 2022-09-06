21bf788ef160acdb8ad41efbb3f100cd940f2bc7
Daffa Azka <destrcode@gmail.com>
2022-09-06 21:21:38 +0700

    sixth commit

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import '../constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -70,10 +71,14 @@ class _LoginViewState extends State<LoginView> {
                       .pushNamedAndRemoveUntil(notesRoute, (route) => false);
                 } on FirebaseAuthException catch (e) {
                   if (e.code == "user-not-found") {
-                    devtools.log("User Not Found");
+                    await showErrorDialog(context, "User Not Found");
                   } else if (e.code == "wrong-password") {
-                    devtools.log("Wrong Password");
+                    await showErrorDialog(context, "Wrong Credentials");
+                  } else {
+                    await showErrorDialog(context, "Error: ${e.code}");
                   }
+                } catch (e) {
+                  e.toString();
                 }
               },
               child: const Text("Login")),
